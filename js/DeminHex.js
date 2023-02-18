09b12391300cdef187c47b974877093fd77f14b6
fhmaisonneuve <e2296133@cmaisonneuve.qc.ca>
2023-02-18 18:07:55 -0500

    Alpha bomb

--- js/DeminHex.js
+++ js/DeminHex.js
@@ -5,27 +5,28 @@ class DeminHex {
         this.width = width;
         this.aCells = [];
         this.aCellsPatern = [];
-        this.aPatern=[3, 2, 3, 4];
+        this.aPatern = [3, 2, 3, 4];
+        this.settings_MinesCount = 3;
     }
 
-    setup(){
+    setup() {
         this.initCells();
+        this.initCells_Mines();
         this.initGridLayout();
     }
 
-     initCells(params) {
+    initCells(params) {
         const aPattern = this.aPatern;
         this.aCellsPatern = [];
         for (let i = 0; i < aPattern.length; i++) {
             this.aCellsPatern.push([]);
             for (let j = 0; j < aPattern[i]; j++) {
-                let cell= new Cell(i,j);
+                let cell = new Cell(i, j);
                 this.aCells.push(cell);
                 this.aCellsPatern[i].push(cell);
             }
         }
     }
-
 
     initGridLayout(params) {
         const container = document.querySelector('.container');
@@ -37,12 +38,15 @@ class DeminHex {
             for (let j = 0; j < hexagonPattern[i]; j++) {
                 const hexagon = document.createElement('div');
                 hexagon.classList.add('hexagon');
-                hexagon.attributes.id = `${i} ${j}`;;
+                hexagon.attributes.id = `${i} ${j}`;
+                hexagon.addEventListener('click', this.clickOnCell.bind(this));
+
                 console.log(hexagon.attributes.id);
                 const eSpan = document.createElement('p');
                 eSpan.classList.add('text');
-                eSpan.innerHTML = hexagon.attributes.id;
+                eSpan.classList.add('not-selectable');
 
+                eSpan.innerHTML = hexagon.attributes.id;
                 hexagon.appendChild(eSpan);
                 row.appendChild(hexagon);
             }
@@ -51,4 +55,39 @@ class DeminHex {
     }
     initGridArray(params) {
     }
+
+    initCells_Mines(params) {
+        let iMines = 0;
+        while (iMines < this.settings_MinesCount) {
+            // FH REmoved as i'll use linear array
+            //var iRow = Math.floor(Math.random() * this.options.rows);
+            //var iCol = Math.floor(Math.random() * this.options.cols);
+            var iRand = Math.floor(Math.random() * this.aCells.length);
+            if (this.aCells[iRand].bBomb == false) {
+                this.aCells[iRand].bBomb = true;
+                iMines++;
+            }
+            console.log(this.aCells);
+        }
+    }
+
+    clickOnCell(evt) {
+        // target pointe sur les enfants, currenttarget est parfait
+        console.log('clickOnCell' + evt.currentTarget.attributes.id);
+        let sID = evt.currentTarget.attributes.id;
+        let aPoint = sID.split(' ').map(Number);
+
+        let cell = this.aCellsPatern[0];
+        console.log(aPoint);
+        let cellw = this.aCellsPatern[aPoint[0]][aPoint[1]];
+        if (cellw.bBomb == true) {
+            evt.currentTarget.style.background = '#ff0000';
+            evt.currentTarget.style.fontSize='24px';
+            evt.currentTarget.firstElementChild.innerText='💣'
+            
+        }else{
+            evt.currentTarget.style.background = '#eee';
+        }
+        console.log(cellw);
+    };
 }
--- js/core.js
+++ js/core.js
@@ -3,7 +3,4 @@ var deminHex;
 function newGame(pOptions = {}) {
     deminHex = new DeminHex(pOptions);
     deminHex.setup();
-    
-    //DeminHex.initGridLayout();
-    //deminHex.initGridArray();
 }
